a04ff8f42203e7cd6be1be028163bfc82c13bd2b
ibalram <balramsinghrathore3@gmail.com>
2020-09-28 01:49:27 +0530

    Addded: Create a contact object to add person details

--- src/com/cg/AddressBookMain.java
+++ src/com/cg/AddressBookMain.java
@@ -1,9 +1,40 @@
 package com.cg;
 
+import java.util.*;
+
 public class AddressBookMain {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Address Book Program");
-	}
+		try {
+			System.out.println("Welcome to Address Book Program");
+			System.out.println("--------------------------------");
+			Scanner sc = new Scanner(System.in);
+			Contact contact = new Contact();
+			System.out.println("Want to add contact details: (1. Yes, 2. No)");
+			int option = Integer.parseInt(sc.nextLine());
+			if (option == 2)
+				return;
+			System.out.println("First Name:");
+			contact.setFirstName(sc.nextLine());
+			System.out.println("Last Name:");
+			contact.setLastName(sc.nextLine());
+			System.out.println("Address:");
+			contact.setAddress(sc.nextLine());
+			System.out.println("City:");
+			contact.setCity(sc.nextLine());
+			System.out.println("State:");
+			contact.setState(sc.nextLine());
+			System.out.println("Zip:");
+			contact.setZip(Integer.parseInt(sc.nextLine()));
+			System.out.println("Phone Number:");
+			contact.setPhoneNumber(sc.nextLine());
+			System.out.println("Email:");
+			contact.setEmail(sc.nextLine());
 
+			System.out.println(contact);
+
+		} catch (Exception e) {
+			System.out.println("Entered invalid values. Retry again");
+		}
+	}
 }
---
+++ src/com/cg/Contact.java
@@ -0,0 +1,96 @@
+package com.cg;
+
+public class Contact {
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String city;
+	private String state;
+	private int zip;
+	private String phoneNumber;
+	private String email;
+
+	public Contact() {
+	}
+
+	public Contact(String firstName, String lastName, String address, String city, String state, int zip,
+			String phoneNumber, String email) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+
+	public int getZip() {
+		return zip;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String toString() {
+		return String.format("Name: %s %s\nAddress: %s, %s, %s, %d\nPhone Number: %s\nEmail: %s", firstName, lastName,
+				address, city, state, zip, phoneNumber, email);
+	}
+}
