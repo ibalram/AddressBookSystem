b16b21dc2a856d2ad09faa98d0de0fd9d1705dc3
ibalram <balramsinghrathore3@gmail.com>
2020-09-28 14:07:34 +0530

    Added: Add multiple AddressBooks to the system

--- src/com/cg/AddressBookMain.java
+++ src/com/cg/AddressBookMain.java
@@ -4,8 +4,10 @@ import java.util.*;
 
 public class AddressBookMain {
 	static Scanner sc = new Scanner(System.in);
+	static HashMap<String, AddressBook> addressBookSystem = new HashMap<String, AddressBook>();
 
-	public static void addAddressBookContacts(AddressBook addressBook) {
+	public static void addAddressBookContacts(String addressBookName) {
+		AddressBook addressBook = addressBookSystem.get(addressBookName);
 		System.out.println("Enter the number of contacts to add: ");
 		int count = Integer.parseInt(sc.nextLine());
 		while (count-- > 0) {
@@ -16,7 +18,7 @@ public class AddressBookMain {
 			contact.setLastName(sc.nextLine());
 			String name = contact.getName();
 			if (addressBook.searchByName(name) != null) {
-				System.out.println("Contact for entered name is already exist");
+				System.out.println("Contact for entered name is already exist\n");
 				break;
 			}
 			System.out.println("Address:");
@@ -33,10 +35,12 @@ public class AddressBookMain {
 			contact.setEmail(sc.nextLine());
 			addressBook.addContact(contact);
 		}
-		System.out.println("\nContacts are added");
+		System.out.println("\nContacts are added\n");
+		addressBookSystem.replace(addressBookName, addressBook);
 	}
 
-	public static void editAddressBookContact(AddressBook addressBook) {
+	public static void editAddressBookContact(String addressBookName) {
+		AddressBook addressBook = addressBookSystem.get(addressBookName);
 		System.out.println("Enter name to edit contact: ");
 		System.out.println("First Name:");
 		String firstName = sc.nextLine();
@@ -46,7 +50,7 @@ public class AddressBookMain {
 
 		Contact contact = addressBook.searchByName(name);
 		if (contact == null) {
-			System.out.println("Contact for given name is not found");
+			System.out.println("Contact for given name is not found\n");
 			return;
 		}
 		System.out.println(
@@ -90,10 +94,12 @@ public class AddressBookMain {
 		}
 		addressBook.editContact(name, contact);
 		System.out.println("\nSuccessfully edited");
-		System.out.println(contact);
+		System.out.println(contact + "\n");
+		addressBookSystem.replace(addressBookName, addressBook);
 	}
 
-	public static void deleteAddressBookContact(AddressBook addressBook) {
+	public static void deleteAddressBookContact(String addressBookName) {
+		AddressBook addressBook = addressBookSystem.get(addressBookName);
 		System.out.println("Enter name of the contact to delete: ");
 		System.out.println("First Name:");
 		String firstName = sc.nextLine();
@@ -104,37 +110,73 @@ public class AddressBookMain {
 		Contact contact = addressBook.searchByName(name);
 		if (contact != null)
 			addressBook.editContact(name, null);
-		System.out.println("\nSuccessfully deleted contact");
+		System.out.println("\nSuccessfully deleted contact\n");
+		addressBookSystem.replace(addressBookName, addressBook);
 	}
 
 	public static void main(String[] args) {
-		AddressBook addressBook = new AddressBook();
-		int option;
+		System.out.println("Welcome to Address Book Program");
+		System.out.println("--------------------------------");
 		try {
-			boolean repeat = true;
-			while (repeat) {
-				System.out.println("Welcome to Address Book Program");
-				System.out.println("--------------------------------");
+			int bookOptions = 0;
+			boolean bookRepeat = true;
+			while (bookRepeat) {
 				System.out.println(
-						"Options:\n1 (add new contacts)\n2 (edit existing contact)\n3 (delete a contact)\n4 (view all contact)");
-				option = Integer.parseInt(sc.nextLine());
-				switch (option) {
+						"Main Options:\n1 (add new AddressBook)\n2 (view or open existing AddressBook)\n3 (exit)");
+				bookOptions = Integer.parseInt(sc.nextLine());
+
+				switch (bookOptions) {
 				case 1:
-					addAddressBookContacts(addressBook);
+					System.out.println("Enter the name of address Book:");
+					String newAddressBook = sc.nextLine();
+					addressBookSystem.putIfAbsent(newAddressBook, new AddressBook());
 					break;
 				case 2:
-					editAddressBookContact(addressBook);
+					StringBuilder str = new StringBuilder();
+					int count = 1;
+					for (String key : addressBookSystem.keySet()) {
+						str.append(count + ". " + key + "\n");
+						count++;
+					}
+					if (str.length() == 0) {
+						System.out.println("There is no Address Book. Add New(1) Exit(2) ");
+						break;
+					}
+					System.out.println("List of Existing AddressBooks:\n" + str.toString());
+
+					System.out.println("Enter the name of addressBook you want to work on:");
+					String addressBookName = sc.nextLine();
+					if (!addressBookSystem.containsKey(addressBookName))
+						break;
+					boolean repeat = true;
+					while (repeat) {
+						System.out.println(
+								"\nAddress Book Options:\n1 (add new contacts)\n2 (edit existing contact)\n3 (delete a contact)\n4 (view all contact)\n5 (go out of current AddressBook");
+						int option = Integer.parseInt(sc.nextLine());
+						switch (option) {
+						case 1:
+							addAddressBookContacts(addressBookName);
+							break;
+						case 2:
+							editAddressBookContact(addressBookName);
+							break;
+						case 3:
+							deleteAddressBookContact(addressBookName);
+							break;
+						case 4:
+							System.out.println(addressBookSystem.get(addressBookName));
+							System.out.println("Enter the name of contact to view:");
+							System.out.println(addressBookSystem.get(addressBookName).searchByName(sc.nextLine()) + "\n");
+							break;
+						case 5:
+							repeat = false;
+						}
+					}
 					break;
-				case 3:
-					deleteAddressBookContact(addressBook);
-					break;
-				case 4:
-					System.out.println(addressBook);
+				default:
+					bookRepeat = false;
 					break;
 				}
-				System.out.println("Continue(1) or exit(2):");
-				if (Integer.parseInt(sc.nextLine()) == 2)
-					repeat = false;
 			}
 		} catch (Exception e) {
 			System.out.println("Entered invalid values. Retry again");
